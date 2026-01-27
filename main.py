645a9c4f1bd24f1271b9e8fb691630f1b3e267e6
Uğur Başdaş <99135103+basdasugur@users.noreply.github.com>
2026-01-27 22:23:26 +0300

    Update phone number in sendwhatmsg function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pywhatkit
 
 # Telefon no, Mesaj, Saat, Dakika
 # Örn: Yarın sabah 07:30'da mesaj at
-pywhatkit.sendwhatmsg("+905427242201",
+pywhatkit.sendwhatmsg("+905554443322",
                       "Bu mesajı Python gönderiyor, "
                       "ben hala uyuyorum! 😴",
                       22, 20)
