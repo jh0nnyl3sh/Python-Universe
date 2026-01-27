cbf6f154766cfa6c5d0ed1cf3749dc466f4ebc86
Uğur Başdaş <99135103+basdasugur@users.noreply.github.com>
2026-01-27 22:22:54 +0300

    Send WhatsApp message using pywhatkit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+# Telefon no, Mesaj, Saat, Dakika
+# Örn: Yarın sabah 07:30'da mesaj at
+pywhatkit.sendwhatmsg("+905427242201",
+                      "Bu mesajı Python gönderiyor, "
+                      "ben hala uyuyorum! 😴",
+                      22, 20)
