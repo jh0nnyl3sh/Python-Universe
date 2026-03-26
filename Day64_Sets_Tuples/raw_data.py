a27a7619d68998fad29fe458a5a7bc4b5aa6ecec
basdasugur <basdasugur@gmail.com>
2026-03-26 21:50:55 +0300

    Day 64: Mastered Sets and Tuples for data purification and filtering

---
+++ Day64_Sets_Tuples/raw_data.py
@@ -0,0 +1,5 @@
+# 1- HAM VERİLER
+raw_target_ips = ["192.168.1.10", "10.0.0.5", "192.168.1.10", "172.16.0.2", "10.0.0.5"]
+
+# Bu bir Tuple! (Değiştirilemez). Sunucu IP'leri.
+safe_ips = ("10.0.0.5", "127.0.0.1")
