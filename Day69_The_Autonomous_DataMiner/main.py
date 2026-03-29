b0b60a7dee2dd13a51e793b993358752740cc88e
basdasugur <basdasugur@gmail.com>
2026-03-29 20:40:39 +0300

    feat: Add fetch_users method for API extraction adn JSON parsing

--- Day69_The_Autonomous_DataMiner/main.py
+++ Day69_The_Autonomous_DataMiner/main.py
@@ -9,3 +9,23 @@ class DataMiner:
         self.target_url = target_url
         self.intel_data = [] # -> çekilen veriler burada duracak
     
+    # Method - 1
+    def fetch_users(self):
+        response = requests.get(self.target_url)
+        
+        if response.status_code == 200:
+            raw_users = response.json()            
+            
+            
+            for user in raw_users:
+                temp_dict = {
+                    "Name": user['name'],
+                    "Email": user['email'],
+                    "City": user['address']['city'],
+                    "Phone": user['phone']
+                }
+                
+
+                self.intel_data.append(temp_dict)
+            print("✅ Veriler başarıyla çekildi ve ajanın hafızasına kaydedildi.\n")
+
