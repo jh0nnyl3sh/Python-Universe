b24b0af3856589712f5d5e3e3610f3e352f5ac22
basdasugur <basdasugur@gmail.com>
2026-03-29 20:41:44 +0300

    feat: Implement export_to_csv method and add execution script

--- Day69_The_Autonomous_DataMiner/main.py
+++ Day69_The_Autonomous_DataMiner/main.py
@@ -29,3 +29,25 @@ class DataMiner:
                 self.intel_data.append(temp_dict)
             print("✅ Veriler başarıyla çekildi ve ajanın hafızasına kaydedildi.\n")
 
+    
+    # Method - 2
+    def export_to_csv(self, filename):
+        
+        with open(filename, mode="w", newline="", encoding="utf-8") as file:
+            headers = ["Name", "Email", "City", "Phone"]
+            
+            # Yazıcı 
+            writer = csv.DictWriter(file, fieldnames=headers)
+            
+            # önce en üste başlıkları yazsın
+            writer.writeheader()
+            
+            # sonra bütün listeyi tek seferde dosyaya bassın
+            writer.writerows(self.intel_data)
+        print(f"📁 Dosya başarıyla oluşturuldu: {filename}")
+            
+miner_bot = DataMiner("https://jsonplaceholder.typicode.com/users")
+
+miner_bot.fetch_users()
+        
+miner_bot.export_to_csv("target_intel.csv")
