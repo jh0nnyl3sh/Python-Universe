1c2dd124437a4fe6df4a9e7125629f0ccf6dafb9
basdasugur <basdasugur@gmail.com>
2026-04-10 20:39:38 +0300

    database_manager first part

---
+++ Day79_Database_Manager_Class/database_manager.py
@@ -0,0 +1,22 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("")
+
+
+class Databasemanager:
+    
+    def __init__(self):
+        pass
+            
+    
+    
+    
+    def connect(self):
+        pass
+    
+    
+    def execute_query(self):
+        pass
