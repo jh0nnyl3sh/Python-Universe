83f2a91a4dbaee1b68d2e10065620365246001db
basdasugur <basdasugur@gmail.com>
2026-04-10 20:47:00 +0300

    def __init__

--- Day79_Database_Manager_Class/database_manager.py
+++ Day79_Database_Manager_Class/database_manager.py
@@ -3,19 +3,25 @@ import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("")
 
 
-class Databasemanager:
+
+class DatabaseManager:
     
     def __init__(self):
-        pass
-            
-    
+        self.host = os.getenv("DB_HOST")
+        self.name = os.getenv("DB_NAME")
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASS")
+        self.port = os.getenv("DB_PORT")
+        self.connection = None
+        
     
     
     def connect(self):
         pass
+    
+    
     
     
     def execute_query(self):
