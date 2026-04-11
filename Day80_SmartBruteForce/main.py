b8de6f5a1635643fa541adeed3713aa19ba953a9
basdasugur <basdasugur@gmail.com>
2026-04-11 20:08:24 +0300

    for loop and standart Brute-Force

--- Day80_SmartBruteForce/main.py
+++ Day80_SmartBruteForce/main.py
@@ -1,2 +1,12 @@
 from passwordlist import passwords
 from passwordlist import target_password
+
+
+for password in passwords:
+
+    if password == target_password:
+        print("✅ Sisteme Sızıldı!")
+        break
+    
+    else:
+        print(f"❌ Denenen şifre yanlış: {[password]} ")
