7cdc04126937bfadc02036800807e1a7c47ced6a
basdasugur <basdasugur@gmail.com>
2026-04-12 18:22:37 +0300

    get_html method

--- Day81_OSINTReconTool/osint_scanner.py
+++ Day81_OSINTReconTool/osint_scanner.py
@@ -7,12 +7,14 @@ class OsintScanner:
     def __init__(self, target_url):
         self.target_url = target_url
 
-        self.headers = {"User-Agent": "Monzilla/5.0 (Windows NT 10.0; Win64; x64)"}
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
         
         
     def get_html(self):
-        pass
+        try:
+            response = requests.get(self.target_url, headers=self.headers)
+            return response.text
+        except:
+            return None
     
     
-    def extract_links(self, html_content):
-        pass
