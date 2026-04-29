a8d2cc2d7b4c165eabee40fc14bb418f9569c980
basdasugur <basdasugur@gmail.com>
2026-04-29 21:57:24 +0300

    gorev 2

--- 100-days-challenge/Day86_LIFO/path_solver.py
+++ 100-days-challenge/Day86_LIFO/path_solver.py
@@ -3,11 +3,37 @@ def path_traversal_cozucu(path):
 
     parcalar = path.split('/')
     
-    print(f"[+] Parçalanmış Liste: {parcalar}")
-    return parcalar
+    
+    # Boş bir Stack (Yığın) oluşturuyoruz
+    stack = []
+
+    # Parçaları sırasıyla okumak için bir döngü başlatıyoruz
+    for parca in parcalar:
+        if parca == "" or parca == ".":
+            # Çift slah (//) yan yana gelmişse boş string ("") oluşur.
+            # Tek nokta (.) ise "mevcut dizinde kal" demektir.
+            # Bu yüzden bu durumlarda hiçbir şey yapmıyoruz (pass).
+            pass
+        
+        
+        elif parca == "..":
+            # LIFO ZAMANI: Bir üst klaösre çıkma komutu geldi!
+            # GÖREV 2.1: Eğer 'stack' boş değilse, en son eklenen elemanı çıkar.
+            # İPUCU: 'if stack:' kullanarak içinin dolu olduğunu kontrol et
+            # İPUCU 2: Çıkarmak için stack.pop() metodunu kullan
+            if stack:
+                stack.pop()    
+
+        else:
+            # GÖREV 2.2: Normal bir klasör ismi geldi ('var', 'www' gibi)
+            # Bunu Stack'in en üstüne ekle.
+            # İPUCU: stack.append(parca) metodunu kullan.
+            stack.append(parca)
+
+    print(f"[+] İşlem Sonrası Stack: {stack}")
+    return
 
 
-# Test edelim
+# test
 test_yolu = "var/www/html/../../log/apache2/../nginx"
 path_traversal_cozucu(test_yolu)
-    
