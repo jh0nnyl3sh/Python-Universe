8b13c235d0a17a782d03e3dca288ddb0da96367c
basdasugur <basdasugur@gmail.com>
2026-05-03 00:22:39 +0300

    def practice

---
+++ 100-days-challenge/Day87/employees.py
@@ -0,0 +1,6 @@
+employees = [
+    {"name": "Ali", "department": "IT", "salary": 5000},
+    {"name": "Ayse", "department": "HR", "salary": 4500},
+    {"name": "veli", "department": "IT", "salary": 5500},
+    {"name": "Fatma", "department": "Finance", "salary": 6000}
+]
