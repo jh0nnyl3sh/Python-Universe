c3c3e7baa2c26ca17f4a0615b454f66bb7c1d419
basdasugur <basdasugur@gmail.com>
2026-06-17 09:34:53 +0300

    docs: kod örnekleri ile ilgili bölümün notları girildi.

---
+++ python-2026-100-days-camp/Section-3-Gun-3-Index-ve-Koleksiyonlar/main.py
@@ -0,0 +1,46 @@
+# Index Mantığı
+name = "Johnny Lesh"
+print(name[0])
+print(len(name)-1)
+print(name[-1])
+
+
+
+# Slicing
+# Slicing, bir dizinin veya listenin belirli bir bölümünü almak 
+# için kullanılan bir tekniktir. Python'da slicing,
+# köşeli parantezler [] içinde iki nokta üst üste (:) kullanılarak 
+# yapılır.
+
+# name[start:end:step]
+
+print(name[:3:])
+print(name[3:6:])
+print(name[6:9:2])
+
+# Slicing Örnekleri
+# Slicing örnekleri olarak, bir dizinin veya
+# listenin belirli bir bölümünü almak için kullanılan
+# slicing tekniğini göstermek için birkaç örnek verilebilir.
+# Örneğin, bir dizi veya listeyi belirli bir aralıkta kesmek için
+# slicing kullanılabilir.
+barkod = "ABCD1234EFGH5678IJKL"
+print(barkod[4:8])  # 1234
+
+
+
+# Liste Nedir ?
+# Liste, birden fazla öğeyi tek bir değişkende saklamamıza
+# olanak tanıyan bir veri yapısıdır.
+# Python'da listeler köşeli parantez [] içinde tanımlanır ve
+# öğeler virgülle ayrılır.
+
+myList = [1, 2, 3, 4, 5]
+print(myList)
+print(type(myList))
+
+
+
+# Veri Tipi Dönüştürme
+
+# Daha İleri Seviye Listeler
