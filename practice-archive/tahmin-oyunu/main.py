1c74b69eb95df807b9e93fa350390035ba8c4d87
basdasugur <basdasugur@gmail.com>
2026-06-18 15:10:29 +0300

    tahmin oyunu kodları

--- practice-archive/tahmin-oyunu/main.py
+++ practice-archive/tahmin-oyunu/main.py
@@ -4,4 +4,13 @@ kac_tahmin_hakki_var = 3
 buldum = 0
 
 while tahmin_sayisi < kac_tahmin_hakki_var and buldum == 0:
+    tahmin = int(input("1 ile 10 arasında bir sayı tamin ediniz: "))
+    if tahmin == tuttugum_sayi:
+        print("Tebrikler!")
+        buldum = 1
+    
+    else:
+        print(f"Bilemediniz, Kalan tahmin sayısı: {(kac_tahmin_hakki_var - tahmin_sayisi) -1}")
+        tahmin_sayisi += 1
+        
     
