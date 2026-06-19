e33df6ff478e732acf32ea58d3e99873311504fc
basdasugur <basdasugur@gmail.com>
2026-06-19 13:36:56 +0300

    Example: Accessing itesm of dictionary

--- python-2026-100-days-camp/Section-4-Gun-4-dictionary-and-others/main.py
+++ python-2026-100-days-camp/Section-4-Gun-4-dictionary-and-others/main.py
@@ -35,3 +35,26 @@ dct = {"key1": "value1", "key2": "value2", "key3": "value3"}
 print(dct["key1"]) # value1
 print(dct["key2"]) # value2
 
+# Example
+person = {
+    "first_name": "John",
+    "last_name": "Lesh",
+    "age": 30,
+    "country": "USA",
+    "is_married": True,
+    "skills": ["Python", "JavaScript", "C++"],
+    "address": {
+        "street": "123 Main St",
+        "city": "New York",
+        "state": "NY",
+        "zip_code": "10001"
+    }
+}
+
+print(person["first_name"]) # John
+print(person["last_name"]) # Lesh
+print(person["age"]) # 30
+print(person["country"]) # USA
+print(person["is_married"]) # True
+print(person["skills"]) # ['Python', 'JavaScript', 'C++']
+print(person["address"]) # {'street': '123 Main St', 'city': 'New York', 'state': 'NY', 'zip_code': '10001'} 
