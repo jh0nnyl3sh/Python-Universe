377084c77da4b8e5cebbe6484a4c912bee4cb8f2
basdasugur <basdasugur@gmail.com>
2026-06-19 13:30:58 +0300

    Dictionarys basics

---
+++ python-2026-100-days-camp/Section-4-Gun-4-dictionary-and-others/main.py
@@ -0,0 +1,29 @@
+# Dictionary
+# A dictionary is a collection of unordered, modifiable (mutable)
+# paired (key:value) data type.
+
+# Creating a dictionary
+# To create a dictionary we use curly brackets, {}
+# or the dict() buit-in function.
+
+# syntax
+empty_dict = {}
+# Dictionary with data values
+dct = {"key1": "value1", "key2": "value2", "key3": "value3"}
+
+
+# Example
+person = {
+    "first_name": "John",
+    "last_name": "Lesh",
+    "age": 30,
+    "country": "USA",
+    "is_married": True,
+    "skills": ["Python", "JavaScript", "C++"],
+    "address": {
+        "street": "123 Main St",
+        "city": "New York",
+        "state": "NY",
+        "zip_code": "10001"
+    }
+}
