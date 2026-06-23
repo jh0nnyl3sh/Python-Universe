587472aa56f679c2965fd5d27bcf6bba3eb53d1b
basdasugur <basdasugur@gmail.com>
2026-06-23 10:59:32 +0300

    if problemi

--- python-2026-100-days-camp/Section-6-Gun-6-Kontroller-ve-Donguler/main.py
+++ python-2026-100-days-camp/Section-6-Gun-6-Kontroller-ve-Donguler/main.py
@@ -16,3 +16,23 @@ y = False
 print(x and y)
 print(x or y)
 print(not x)
+
+# Problem
+# Bir sistem için kullanıcıların sahip olduğu puanlara göre üyelik
+# seviyelerini belirleyen bir kod yazalım.
+
+# kullanıcı puanı
+customer_points = 75
+
+# if yapısı
+# 100'e eşit veya daha büyükse 
+if customer_points >= 100:
+    print("Platinum Member")
+    
+# 50 ile 99 arasındaysa
+elif 50 <= customer_points <= 99:
+    print("Gold Member")
+
+# 50'den küçük ise
+else:
+    print("Silver Member")
