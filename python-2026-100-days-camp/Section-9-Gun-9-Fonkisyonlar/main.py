3484e86496c228cfb991ff8ad5a3ad4142060dca
basdasugur <basdasugur@gmail.com>
2026-06-27 20:59:19 +0300

    Fonksiyon Örnekleri

--- python-2026-100-days-camp/Section-9-Gun-9-Fonkisyonlar/main.py
+++ python-2026-100-days-camp/Section-9-Gun-9-Fonkisyonlar/main.py
@@ -15,3 +15,19 @@ def sum_example(num1, num2):
     print(num1 + num2)
     
 sum_example(5, 10)
+
+# pratik fonksiyonlar
+
+def divideNumber(number):
+    return number / 2
+
+print(divideNumber(20))
+
+myList = [3,5,7,10,20,30]
+
+myResultList = []
+
+for num in myList:
+    myResultList.append(divideNumber(num))
+
+print(myResultList)
