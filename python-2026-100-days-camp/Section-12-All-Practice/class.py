1e16211dd0b6f117a435465e9a710a2933d8d44a
basdasugur <basdasugur@gmail.com>
2026-07-03 21:32:25 +0300

    Class örneği: araba classı kurup, class üzerinden bir nesne oluşturup, sonra da while göndüsü içinde bu nesneyi çalıştırıp istediğimiz yerde break komutu ile çıktı.

--- python-2026-100-days-camp/Section-12-All-Practice/class.py
+++ python-2026-100-days-camp/Section-12-All-Practice/class.py
@@ -11,3 +11,18 @@ class Car:
     def sur(self):
         if self.yakit_seviyesi < 10:
             print("Yakıtınız bitti, lütfen benzin alın!")
+        else:
+            print(f"Araba sürülüyor... Kalan yakıt: {self.yakit_seviyesi}")
+            self.yakit_seviyesi -= 10
+            
+            
+araba1 = Car("BMW", "X5", 100)
+
+
+
+while True:
+    
+    araba1.sur()
+    if araba1.yakit_seviyesi < 10:
+        print("Yakıtınız bitti, lütfen benzin alın!")
+        break
