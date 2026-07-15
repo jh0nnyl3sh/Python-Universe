e606495597732ce4dca781fe282b171f86f974dd
basdasugur <basdasugur@gmail.com>
2026-07-15 12:34:20 +0300

    pdf to docx

--- python-2026-100-days-camp/Section-13-Gun-14-Moduller-ve-Paketler/pdfto.py
+++ python-2026-100-days-camp/Section-13-Gun-14-Moduller-ve-Paketler/pdfto.py
@@ -1,3 +1,11 @@
 from pdf2docx import Converter
 
 
+pdf_path = 'ugurpdf.pdf'
+
+docx_path = "ugurpdf.docx"
+
+cv = Converter(pdf_file = pdf_path)
+cv.convert(docx_file=docx_path, start=0, end=None)
+
+cv.close()
