71db32320fdf89459b78bf0871da295d9323e8e4
basdasugur <basdasugur@gmail.com>
2026-07-16 14:10:10 +0300

    QR oluşturucu kaynak kodu

--- python-2026-100-days-camp/Section-13-Gun-14-Moduller-ve-Paketler/createqr.py
+++ python-2026-100-days-camp/Section-13-Gun-14-Moduller-ve-Paketler/createqr.py
@@ -1,2 +1,20 @@
 # QR Oluşturucu
 
+import pyqrcode
+import png
+
+
+
+def create_qr_code(data, filename):
+    # QR kodu oluştur
+    qr = pyqrcode.create(data)
+    
+    # QR kodunu PNG formatında kaydet
+    qr.png(filename, scale=6)
+    
+    print(f"QR kodu '{filename}' olarak kaydedildi.")
+
+
+
+
+create_qr_code("https://www.example.com", "example_qr.png")
