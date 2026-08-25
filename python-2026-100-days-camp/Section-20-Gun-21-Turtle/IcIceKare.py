1d89ed863b90d26b27bf2aa58ff1d10338e157cd
basdasugur <basdasugur@gmail.com>
2026-08-25 10:43:49 +0300

    Turtle ile sarmal kare çizimi

---
+++ python-2026-100-days-camp/Section-20-Gun-21-Turtle/IcIceKare.py
@@ -0,0 +1,26 @@
+import turtle
+
+
+tahta = turtle.Screen()
+tahta.bgcolor("yellow")
+tahta.title("Turtle ile Şekil Çizimi")
+
+
+
+
+cizim = turtle.Turtle()
+cizim.forward(30)
+cizim.left(90)
+cizim.forward(30)
+cizim.left(90)
+cizim.forward(60)
+cizim.left(90)
+cizim.forward(60)
+cizim.left(90)
+cizim.forward(90)
+cizim.left(90)
+cizim.forward(90)
+cizim.left(90)
+cizim.forward(120)
+
+turtle.done()
