7603ead2b8565c69fe82935bbda9c303fc74ef39
basdasugur <basdasugur@gmail.com>
2026-08-26 12:20:18 +0300

    turtle spiral çalışması

---
+++ python-2026-100-days-camp/Section-20-Gun-21-Turtle/spiral.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle_scn = turtle.Screen()
+turtle_scn.bgcolor("black")
+turtle_scn.title("Turtle ile Spiral Çizimi")
+
+turtle_instance = turtle.Turtle()
+turtle_instance.color("red")
+
+turtle_instance.circle(100)
+
+turtle.done()
+
