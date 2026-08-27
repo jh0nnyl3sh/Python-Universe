8976e1285422bdc0c6937196ed3871f080cf4cba
basdasugur <basdasugur@gmail.com>
2026-08-27 13:49:10 +0300

    Interactive drawing movement for

--- python-2026-100-days-camp/Section-20-Gun-21-Turtle/InteractiveDrawing.py
+++ python-2026-100-days-camp/Section-20-Gun-21-Turtle/InteractiveDrawing.py
@@ -6,13 +6,22 @@ drawing_board.title("Interactive Drawing Board")
 
 turtle_instance = turtle.Turtle()
 
+# Forward movement = Space 
 def turtle_forward():
     turtle_instance.forward(100)
+
+# Left movement = Left    
+def turtle_left():
+    turtle_instance.left(90)
     
+# Right movement = Right
+def turtle_right():
+    turtle_instance.right(90)
 
 drawing_board.listen()
 drawing_board.onkey(fun=turtle_forward, key="space")
-
+drawing_board.onkey(fun=turtle_left, key="Left")
+drawing_board.onkey(fun=turtle_right, key="Right")
 
 turtle.mainloop()
 
