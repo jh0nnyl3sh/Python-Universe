dd1ebe68deff9cc91efef517fea05e3a57479ef6
basdasugur <basdasugur@gmail.com>
2026-08-29 12:29:13 +0300

    examples turtles

--- python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
+++ python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
@@ -1,11 +1,8 @@
-import turtle # -> Import the turtle module
+import turtle as t
+from random import random
 
-draw_board = turtle.Screen() # -> Create a screen object for the turtle graphics window
-draw_board.bgcolor("lightblue") # -> Set the background color of the turtle graphics window to light blue
-draw_board.title("Catch the Turtle Game") # -> Set the title of the turtle graphics window to "Catch the Turtle Game"
-
-turtle_instance = turtle.Turtle() # -> Create a turtle object named turtle_instance
-
-turtle_instance.forward(100) # -> Move the turtle_instance forward by 100 units
-
-turtle.done() # -> Keep the turtle graphics window open until it is manually closed
+for i in range(100):
+    steps = int(random() * 100)
+    angle = int(random() * 360)
+    t.right(angle)
+    t.fd(steps)
