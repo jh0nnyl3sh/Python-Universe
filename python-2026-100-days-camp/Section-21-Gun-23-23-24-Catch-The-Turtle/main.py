b43dcedfec7ca80b20873db40ee32ea59531a8ec
basdasugur <basdasugur@gmail.com>
2026-08-29 14:58:03 +0300

    drawing angel with shape of turtle

--- python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
+++ python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
@@ -1,9 +1,12 @@
+"""
 import turtle
 
 # turtle shape("turtle")
 t = turtle.Turtle()
 
 t.shape("turtle")
+
+
 
 def fxn(x, y):
     t.right(90)
@@ -14,3 +17,24 @@ t.forward(100)
 t.onclick(fxn)
 
 turtle.done()
+"""
+
+
+import turtle
+
+
+
+x = turtle.Turtle()
+y = 5 # Kenar sayısı
+z = 50 # kenar uzunluğu
+a = 360.0 / y  # daireyi bölen açı
+x.shape("turtle") # turtle şekli
+
+for i in range(y):
+    x.forward(z) # z kadar ileri git (kenar uzunluğu)
+    x.right(a) # a kadar sağa dön (açı)
+    
+turtle.done()
+
+
+
