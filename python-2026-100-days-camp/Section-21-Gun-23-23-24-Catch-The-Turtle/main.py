3d036dc8330b56d893c3d93eb0014fc2bafe19a2
basdasugur <basdasugur@gmail.com>
2026-09-10 19:15:32 +0300

    hide turtle

--- python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
+++ python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
@@ -9,6 +9,8 @@ FONT = ("Arial", 30, "bold")
 
 #score turtle
 score_turtle = turtle.Turtle()
+
+score_turtle.hideturtle()
 score_turtle.color("dark blue")
 score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
 
