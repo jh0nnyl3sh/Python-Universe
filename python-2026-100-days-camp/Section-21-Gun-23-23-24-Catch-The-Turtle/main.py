99473bd0ea08edb36332bb2bd92f79d0de9fe872
basdasugur <basdasugur@gmail.com>
2026-09-12 21:54:44 +0300

    def setup_score_turtle():

--- python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
+++ python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
@@ -10,18 +10,20 @@ FONT = ("Arial", 30, "bold")
 #score turtle
 score_turtle = turtle.Turtle()
 
-score_turtle.hideturtle()
-score_turtle.color("dark blue")
-score_turtle.penup()
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color("dark blue")
+    score_turtle.penup()
 
 
-top_hight = screen.window_height() / 2
-y = top_hight * 0.8
+    top_hight = screen.window_height() / 2
+    y = top_hight * 0.8
 
-score_turtle.setpos(0, y)
-score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
+    score_turtle.setposition(0, y)
+    score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
 
 
+setup_score_turtle()
 
 
 
