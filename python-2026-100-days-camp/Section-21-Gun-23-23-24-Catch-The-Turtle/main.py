d73ccd7fa787b880891263b3582a0dac0daff8dd
basdasugur <basdasugur@gmail.com>
2026-09-12 21:52:13 +0300

    score table

--- python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
+++ python-2026-100-days-camp/Section-21-Gun-23-23-24-Catch-The-Turtle/main.py
@@ -12,6 +12,13 @@ score_turtle = turtle.Turtle()
 
 score_turtle.hideturtle()
 score_turtle.color("dark blue")
+score_turtle.penup()
+
+
+top_hight = screen.window_height() / 2
+y = top_hight * 0.8
+
+score_turtle.setpos(0, y)
 score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
 
 
