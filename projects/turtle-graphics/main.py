87f5ba59a7b6c955c1be57d7ce842acbe6e90152
basdasugur <basdasugur@gmail.com>
2026-09-15 08:20:53 +0300

    upgrade projects

--- projects/turtle-graphics/main.py
+++ projects/turtle-graphics/main.py
@@ -1,9 +1,22 @@
 import turtle as t
+
 t.speed(0)
+
 t.bgcolor("black")
+
 t.color("aqua")
+
 t.hideturtle()
-for i in range(170):
-    t.circle(i-5)
+
+for i in range(150):
+    t.circle(i-1)
     t.left(5)
+    
+    
+    t.color("red")
+    for j in range(150):
+        t.circle(j-1)
+        t.left(5)
+
+
 t.done()
