c7def86411aa173ec6bf16d70335c48ca76230c8
basdasugur <basdasugur@gmail.com>
2026-09-18 20:31:34 +0300

    added while loop

--- geeksforgeeks-data-science-projects/web-scraping-projects/1-quote-scraping/main.py
+++ geeksforgeeks-data-science-projects/web-scraping-projects/1-quote-scraping/main.py
@@ -10,3 +10,14 @@ all_quotes = []
 # this part of the url is constant
 base_url = "http://quotes.toscrape.com/"
 
+# this part of the url will keep changing
+url = "/page/1"
+
+while url:
+    
+    # concatenating both urls
+    # making request
+    res = requests.get(f"{base_url}{url}")
+    print(f"Now Scraping:{base_url}{url}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    
